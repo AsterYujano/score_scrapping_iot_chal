6323b627adab3e6614d77eff48ca5a49fcad932b
François <asteryujano@gmail.com>
2019-05-22 01:03:10 +0200

    save

---
+++ score_scrap.py
@@ -0,0 +1,57 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import re
+
+# comment ... I should sleep now :(
+
+domain_name = 'https://www.iotchallengekeysight.com'
+project_urls = []
+votes = []
+
+#browse through pages
+
+print('---------------------------------------')
+print('-  Starting project URL gathering...  -')
+print('---------------------------------------')
+for page in range(0, 180, 12):
+	print('[+] Crawling around the '+str(page)+'th entry...')
+	url = 'https://www.iotchallengekeysight.com/2019/entries?start='+str(page)
+
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+
+	cards = soup.find_all('div', attrs={'class': 'card', 'itemprop': 'blogPost'})
+
+	for card in cards:
+		link_url = card.find('a', href=True, itemprop='url')['href']
+		project_urls.append(link_url)
+		#time.sleep(0.5)
+print('[o] Complete')
+
+print('-------------------------------')
+print('-  Starting Vote fetching...  -')
+print('-------------------------------')
+count = 1
+for project_url in project_urls:
+	#counter
+	print(str(count) + ' / ' + str(len(project_urls)), end='')
+	print('\r', end='')
+	count = count + 1
+
+	url = domain_name+project_url
+
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+
+	header = soup.find('div', attrs={'class': 'page-header'})
+	vote_string = header.find('h5').text
+	vote = int(re.sub("[^0-9]", "", vote_string))
+	votes.append(vote)
+print('[o] Complete')
+print()
+print('Highest : ' + str(max(votes) + 'votes')
+print('Lowest : ' + str(max(votes) + 'votes')
+print()
+print(votes)
